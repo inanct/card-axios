0f7186b58454ff8d2aee11d486b99371d8b7c147
inanct <inanc.tekinoktay@gmail.com>
2023-12-06 22:02:18 +0300

    card-axios-project

---
+++ src/components/NavBarCom.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
+
+const NavBarCom = () => {
+  return (
+    <Navbar sticky='top' bg='primary' variant='dark'>
+    <Container>
+        <Navbar.Brand href='#home'>Contact Form</Navbar.Brand>
+        
+    </Container>
+    </Navbar>
+  )
+}
+
+export default NavBarCom
